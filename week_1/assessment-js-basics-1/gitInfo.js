00dc24b050adeba918daab1d923449badac9bf0a
elise <115508722+cultelise@users.noreply.github.com>
2023-04-07 16:02:38 -0500

    Add assessment js basics 1. 50% completion point.

---
+++ week_1/assessment-js-basics-1/gitInfo.js
@@ -0,0 +1,83 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+let gitDefinition =
+  'Git is an organizational technology designed to streamline the software development process via a multi-lateral version management process.';
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+let gitHubDefinition =
+  'GitHub is a website which utilizes git to provide users with a one-stop "hub" for managing git-enabled projects on the internet.';
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+let gitInitDefinition =
+  '"Git init" initializes a local folder/project as a git repository, enabling it to utilize everything git has to offer.';
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
---
+++ week_1/assessment-js-basics-1/sdlc.js
@@ -0,0 +1,31 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning =
+  'This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.';
+const analysis =
+  'The second step, analysis of requirements, involves discovering/determining ways the plan can be implemented in accordance with established business principles and collected data sets. ';
+const design =
+  'In the third step, design, the "how" of the development process is approached. During this step, a software "blueprint" is drafted, analyzed, and agreed upon.';
+const implementation =
+  'The fourth step involves the nitty gritty of the software development process. The "house" of the software project is built (coded) in accordance with the design plan.';
+const testing =
+  'The fifth step, testing & integration, puts the newly implemented application through the ringer. This step ensures that the app works properly and runs well in its designated environment.';
+const maintain =
+  'Finally, in the sixth step, the maintenance process consists of collecting feedback, debugging, and updating the application in order to keep it fully functional within the ever-advancing world of software.';
